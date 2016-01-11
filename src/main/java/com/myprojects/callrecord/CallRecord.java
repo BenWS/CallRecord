de902236aa5b3a61a5781aabdcad67544e65150c
Ben <shippeyben@gmail.com>
2016-01-11 17:48:17 -0500

    Ensuring code is up to date

--- src/main/java/com/myprojects/callrecord/CallRecord.java
+++ src/main/java/com/myprojects/callrecord/CallRecord.java
@@ -18,7 +18,6 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
